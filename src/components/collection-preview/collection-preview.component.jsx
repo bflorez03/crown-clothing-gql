e0eef202b4caa8a5cf611bf5afb68b37c0fbc88f
Brayan Florez <bflorez@outlook.com>
2021-02-13 13:32:02 +0100

    collection items

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -1,16 +1,11 @@
 import React from 'react';
 import { withRouter } from 'react-router-dom';
-
-import CollectionItem from '../collection-item/collection-item.component';
-
+import { default as CollectionItem } from '../collection-item/collection-item.container';
 import './collection-preview.styles.scss';
 
 const CollectionPreview = ({ title, items, history, match }) => (
   <div className='collection-preview'>
-    <h1
-      className='title'
-      onClick={() => history.push(`${match.path}/${title.toLowerCase()}`)}
-    >
+    <h1 className='title' onClick={() => history.push(`${match.path}/${title.toLowerCase()}`)}>
       {title.toUpperCase()}
     </h1>
     <div className='preview'>
